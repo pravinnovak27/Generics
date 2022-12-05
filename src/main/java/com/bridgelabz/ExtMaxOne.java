7ac832d853e6bbb40c436821153a9b7ec150b91d
Pravin Novak <pravinnovak2707@gmail.com>
2022-12-06 02:32:57 +0530

    added the max method for three parameters

---
+++ src/main/java/com/bridgelabz/ExtMaxOne.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+public class ExtMaxOne <T extends Comparable<T>> {
+    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+
+        T max = val1;
+
+        /*
+        check condition
+         */
+        if (val2.compareTo(max) > 0 ) {
+            max = val2;
+        } if (val3.compareTo(max) > 0){
+            max = val3;
+        }if (val4.compareTo(max) > 0 ){
+            max = val4;
+        }if (val5.compareTo(max) > 0){
+            max = val5;
+
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+// create object for FindMaximum class object name is max
+
+        new ExtMaxOne<>();
+
+        //print maximum value
+        System.out.println( "The maximum value is :: "+maximumValue(2.5f,3.5f,4.5f,5.5f,6.5f));
+
+
+    }
+}
